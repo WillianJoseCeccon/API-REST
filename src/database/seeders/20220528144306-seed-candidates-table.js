2b97d99cc6c5bd88810bdc9026a581419c7a6957
willianjosececcon <willianjosececcon@gmail.com>
2022-05-28 12:06:01 -0300

    configurando os seeders, migration e criando a retorno do get para candidates

---
+++ src/database/seeders/20220528144306-seed-candidates-table.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('candidates', [{
+      name: 'Arthur',
+      bio: 'I am a full stack javascript developer with experience on mern stack.',
+      email: 'arthur@gmail.com',
+      phone: '1111-1111',
+      open_to_work: true,
+      created_at: new Date(),
+      updated_at: new Date()
+    },
+    {
+      name: 'willian',
+      bio: 'I am a full stack javascript developer with experience on mern stack.',
+      email: 'willian@gmail.com',
+      phone: '2222-1111',
+      open_to_work: true,
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+  ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('candidates', null, {})
+  }
+};
